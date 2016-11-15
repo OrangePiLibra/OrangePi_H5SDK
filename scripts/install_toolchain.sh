1a99a870995128345c14b23237c2091774dbf3a4
BuddyZhang1 <buddy.d.zhang@gmail.com>
2016-11-15 16:46:09 +0800

    [PATCH 1/2] toolchain: add scripts to build cross_toolchain

---
+++ scripts/install_toolchain.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+#####################################
+##
+## Install cross toolchain
+#####################################
+
+if [ -z $TOOT ]; then
+	ROOT=`cd .. && pwd`
+fi
+
+TOOLS="$ROOT/toolchain"
+TOOLTARXZ="$ROOT/external/toolchain_tar/toolchain"
+TOOLTAR="$ROOT/external/toolchain.tar.gz"
+
+echo "Uncompress toolchain.. "
+cat ${TOOLTARXZ}* > ${TOOLTAR}
+
+tar xzvf $TOOLTAR -C $ROOT
+
+
