455d03b15e0c963781ff0afcb021a6977b4936a3
BuddyZhang1 <buddy.d.zhang@gmail.com>
2016-12-10 10:36:19 +0800

    [PATCH] dts: add scripts to compile original dts

--- scripts/kernel_compile.sh
+++ scripts/kernel_compile.sh
@@ -79,12 +79,14 @@ fi
 if [ $BUILD_KERNEL = "1" ]; then
 	# compile dts
 	echo -e "\e[1;31m Start Compile DTS \e[0m"
+	make -C $LINUX ARCH=arm64 CROSS_COMPILE=$TOOLS -j4 dtbs
 	#$ROOT/kernel/scripts/dtc/dtc -Odtb -o "$BUILD/OrangePiH5.dtb" "$LINUX/arch/arm64/boot/dts/${PLATFORM}.dts"
 	## DTB conver to DTS
 	# Command:
 	# dtc -I dtb -O dts -o target_file.dts source_file.dtb
 	########
 	# Update DTB with uboot
+	echo -e "\e[1;31m Cover sys_config.fex to DTS \e[0m"
 	cd $ROOT/scripts/pack/
 	./pack
 	cd -
